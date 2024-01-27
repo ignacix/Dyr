3816b883a3d7c6f8e53a10b813cfef20c21cb607
ignacix <ignanj8@gmail.com>
2024-01-26 23:27:44 -0300

    feat: Se agrega la la librería FluentValidation

---
+++ dyr/Modelo/PalabraValidation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+namespace Modelo
+{
+    public class PalabraValidation : AbstractValidator<Palabra>
+    {
+        public PalabraValidation() 
+        {
+            RuleFor(palabra => palabra.word).NotNull();
+        }
+    }
+}
